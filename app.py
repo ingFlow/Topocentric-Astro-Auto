69348cc99026f3f0559f67140266214e30984ab9
Tessa-777 <tessakite777@gmail.com>
2024-09-16 19:33:55 +0000

    GUI templated - now it updates midlle top row when any rgp/list item is selected as desired

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
-from flask import Flask, render_template, request
-import aspects_implementation as asp
-import main_converge as mc
-from datetime import datetime, timedelta
+from flask import Flask, render_template, jsonify, request
+import pd_automate 
+import main_converge
+import julian
 
 app = Flask(__name__)
 
-# Simulated function that returns a list of datetimes
-def get_datetimes():
-    dt_radix_start, dt_radix_end, geopos, list_of_events = mc.get_json_birth_data('data_input/jacquiline onassis.json')
-    file_path = '9_14_ver1_sorted_planet_data.csv'
-    dt_day_before_rad = dt_radix_end - timedelta(days=1)
-    start_date_str = dt_day_before_rad.strftime('%d %B %Y')
-
-    list_times_to_process = asp.process_csv(file_path, start_date_str,100, -4)
-    return list_times_to_process
-
 @app.route('/')
-def index():
-    # Get the list of datetimes from the function
-    list_times_to_process = get_datetimes()
-    return render_template('index.html', datetimes=list_times_to_process)
+def home():
+    dt_actual_dob, _, _, geopos, list_of_events = main_converge.get_json_birth_data("data_input/jacquiline onassis.json")
+    jd_radix = julian.to_jd(dt_actual_dob)
+    list_dt_events = [t[0] for t in list_of_events]
+    str_asp_dir, str_asp_conv = pd_automate.pd_for_time_event_norad(jd_radix,julian.to_jd(list_dt_events[0]),geopos)
+    list_all_asp = [*str_asp_dir.split('\n'),*str_asp_conv.split('\n')]
+    
+
+    left_items = list_dt_events
+    right_items = list_all_asp
+    return render_template('index.html', left_column_items=left_items, right_column_items=right_items)
+
+@app.route('/update_content')
+def update_content():
+    left_radio = request.args.get('left_radio', '')
+    right_radio = request.args.get('right_radio', '')
+    left_item = request.args.get('left_item', '')
+    right_item = request.args.get('right_item', '')
+
+    content = f"Left Radio: {left_radio}, Right Radio: {right_radio}, Left Item: {left_item}, Right Item: {right_item}"
+    
+    return jsonify({'message': content})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
